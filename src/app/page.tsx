98dbff0071dbd675680a05b95d00469dad2c5d1d
platinumpizza29 <kbelgi1@gmail.com>
2024-12-09 09:08:48 +0000

    feat: add interactive globe component
    Implement the Cobe component to render an interactive globe using 
    the Cobe library. The component handles window resizing and 
    animation, enhancing the visual experience. Additionally, remove 
    the TypeScript version of the component and clean up the 
    HomePage layout by adjusting padding.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,7 @@ export default function HomePage() {
     <main className="min-h-[100dvh]">
       <NavbarComp />
       <div className="grid grid-cols-1 md:grid-cols-2 place-items-center">
-        <div className="p-10 break-words">
+        <div className="p-10">
           <h1 className="text-4xl md:text-6xl font-bold">Hero goes here</h1>
           <p className="">Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam, sit. Nihil assumenda atque nesciunt, facere exercitationem aliquid nisi libero, ullam cupiditate saepe ducimus voluptatibus impedit quae, velit incidunt reiciendis tempore?</p>
         </div>
