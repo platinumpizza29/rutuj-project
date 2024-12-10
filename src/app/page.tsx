642277175f3c4d3c196edb9625c24b3100dd177f
platinumpizza29 <kbelgi1@gmail.com>
2024-12-10 12:54:21 +0000

    feat: add masonry section component to homepage
    Introduce the MansorySectionComp to the homepage layout, enhancing 
    visual appeal and content organization. This new section displays 
    a grid of images with descriptive overlays improving user 
    engagement and showcasing relevant themes.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import FeaturesComp from "~/components/features";
 import FooterComp from "~/components/footerComp";
 import HeroSection from "~/components/heroSection";
+import MansorySectionComp from "~/components/masoryComp";
 import NavbarComp from "~/components/navbarComp";
 import PricingComp from "~/components/pricingComp";
 
@@ -13,6 +14,9 @@ export default function HomePage() {
       </section>
       <section>
         <FeaturesComp />
+      </section>
+      <section>
+        <MansorySectionComp />
       </section>
       <section className="bg-primary-foreground">
         <PricingComp />
